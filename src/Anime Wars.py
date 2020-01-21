38100358a1c437f92ac877d4f39755a8fad85382
mdaniel-devonte <57999069+mdaniel-devonte@users.noreply.github.com>
2020-01-21 09:40:12 -0500

    Update Anime Wars.py

--- src/Anime Wars.py
+++ src/Anime Wars.py
@@ -1,35 +1,52 @@
+import random
 import sys
 import pygame
 pygame.init()
 width, height = 400, 300
 screen = pygame.display.set_mode( (width, height) )
-deck1=[]
-discard1=[]
-deck2=[]
-discard2=[]
-class Card:
+
+class Cards:
     def __init__ (self,suit,value,img):
     #Constructor for Card class
         self.suit = suit
         self.value = value
         self.img = pygame.image.load("src/img/DIO.jpg")
-    #showing cards
-    def draw():
-        p1card=deck1.pop()
-        discard1.append(plcard)
-        
-    #Compare cards
-    def compare (self, other_card):
-        if self > other_card:
-            self wins
-        elif self < other_card
-            other_card wins
+#Create the deck and hands
+class Decks:
+    #Constructor for the decks
+    def __init__ (self, name):
+        self.deck = []
+    #The full deck with 52 cards        
+    def full_deck(self): 
+        suits=["Sp", "He", "Cl", "Di"] 
+        for suit in suits:
+            for value in range (1,14):
+                #Use the cards class to create cards
+                self.deck.append(Cards(suit, value, img)) 
+        random.shuffle(deck)
+        return deck
 
+    def player_hands(deck):
+        hand1 = deck[:len(deck)/2]
+        hand2 = deck[len(deck)/2:]
+       
+discard1 = []
+discard2 = []
+
+    
 Dio=Card("Cl", 13,)
 Vegeta=Card("Di", 13)
+#showing cards
+def draw():
+    p1card=hand1.pop()
+    discard1.append(plcard)
+    p2card = hand2.pop()
+    discard2.append(p2card)    
+    #Compare cards
+def compare (self, other_card):
+    if self > other_card:
+         self wins
+    elif self < other_card
+        other_card wins
 
 #Automate the picking of cards
-deck=[]
-    for suit in ["Cl", "Di", "He", "Sp"]:
-        for value in range (1,14):
-            deck.append(Card(suit, value))
